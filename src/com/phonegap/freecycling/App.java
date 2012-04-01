79f8846e1b236c5d993424ad840ff0f36fa2f9ca
Maysam <kherad@gmail.com>
2012-04-01 22:40:11 +0800

    moving to git

---
+++ src/com/phonegap/freecycling/App.java
@@ -0,0 +1,14 @@
+package com.phonegap.freecycling;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+
+public class App extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
